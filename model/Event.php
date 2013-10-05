806c3621d372efe7747489e841739ea3be815244
Osman Dandia <odandia@gmail.com>
2013-10-04 18:52:03 -0700

    Code cleanup

--- model/Event.php
+++ model/Event.php
@@ -6,22 +6,23 @@
 	Use :: Event Class Used For  Event Data
 	*/
 	
-class Event extends common{
+class Event extends common
+{
 
-	public $eventId = 1;
-	
-	public function addEventData()
-	{
-	 //var_dump($data);exit;
-    $date = date("y-m-d h:m:s");
-   $qry = "INSERT INTO event (id, name, description, from_date, to_date, address,
-     city, state, country, zipcode, is_active, createdat, updatedat) 
-     VALUES (1, 'Marketingcamp chicago', 'This is Test For Marketingcamp Chicago',
-     '$date', '$date', '200 S. Wacker Drive, 15th Floor',
-    'Chicago', 'IL', 'USA', '60606', '1', '$date', '$date');";
-    
-    $GLOBALS['o__objDB']->db_altertable($qry);
-  }
+    public $eventId = 1;
+
+    public function addEventData()
+    {
+        $date = date("y-m-d h:m:s");
+        
+        $qry = "INSERT INTO event (id, name, description, from_date, to_date, address,
+        city, state, country, zipcode, is_active, createdat, updatedat) 
+        VALUES (1, 'Marketingcamp chicago', 'This is Test For Marketingcamp Chicago',
+        '$date', '$date', '200 S. Wacker Drive, 15th Floor',
+        'Chicago', 'IL', 'USA', '60606', '1', '$date', '$date');";
+
+        $GLOBALS['o__objDB']->db_altertable($qry);
+    }
 }
 
 ?>
--- model/geocoder.php
+++ model/geocoder.php
@@ -1,25 +1,28 @@
 <?php 
 class geocoder{
+
     private $url = "https://maps.google.com/maps/api/geocode/json";
-    private $address ;
+    private $address;
+    
     public function __construct($address = null)
     {
-      if($address !== null){
-			  $this->address = $address;
-		  }
+        if($address !== null){
+            $this->address = $address;
+        }
     }
-     public function getResult(){
-      
+    public function getResult() {
+
         $resp_json = file_get_contents($this->url . "?address=".urlencode($this->address)."&sensor=false");
         $resp = json_decode($resp_json, true);
         $result =  array();
-        if($resp['status']=='OK'){
+
+        if ($resp['status']=='OK') {
             $result['formatted_address'] = $resp['results'][0]['formatted_address'];
             $result['geometry'] = $resp['results'][0]['geometry']['location'];
             return $result;
             
-        }else{
-           // if no result is found 
+        } else {
+            // if no result is found 
             return false;
         }
     }
